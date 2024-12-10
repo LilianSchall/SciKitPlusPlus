f22f2ad7c6c56bdeed8775bc14f732a78d00f43f
Lilian Schall <lilian.schall@epita.fr>
2024-12-10 21:53:10 +0100

    fix: added test to compute overhead of loading mnist data

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
+all: download-examples build-compute
+
+download-examples:
+	wget https://s3l4h.com/static/examples.zip && mv examples.zip tests && cd tests && unzip examples.zip && rm examples.zip
+
 build-compute:
-	cmake -S ./ -B build/ -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+	cmake -S ./ -B build/ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && cp -r tests/examples build
+
 clean:
-	rm -rf build compile_commands.json
+	rm -rf build compile_commands.json tests/examples
--- tests/mnist_test.cc
+++ tests/mnist_test.cc
@@ -8,6 +8,26 @@
 #include <sk-plusplus/tensor/tensor.hh>
 #include <vector>
 
+TEST(MnistTest, DatasetTest)
+{
+    sk::serializer::TensorSerializer serializer;
+
+    sk::Tensor X = serializer.deserialize("examples/x_mnist.dat");
+    X /= 255.0f;
+    sk::Tensor y = serializer.deserialize("examples/y_mnist.dat");
+
+    sk::Tensor w1 = serializer.deserialize("examples/w1.dat");
+    sk::Tensor w2 = serializer.deserialize("examples/w2.dat");
+
+    sk::Tensor b1 = serializer.deserialize("examples/b1.dat");
+    sk::Tensor b2 = serializer.deserialize("examples/b2.dat");
+
+    EXPECT_EQ(X.shape[0], 60000);
+    EXPECT_EQ(X.shape[1], 784);
+
+    EXPECT_EQ(y.shape[0], 60000);
+}
+
 TEST(MnistTest, AccuracyTest)
 {
     sk::serializer::TensorSerializer serializer;
