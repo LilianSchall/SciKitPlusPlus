0c16c5e2ef71ac891022c36946fc61ee5045bedd
Lilian Schall <lilian.schall@epita.fr>
2024-12-10 14:02:12 +0100

    feat: added inverse transformation to all scalers

--- sk-plusplus/sk-plusplus/scaler/min_max_scaler.cc
+++ sk-plusplus/sk-plusplus/scaler/min_max_scaler.cc
@@ -2,13 +2,27 @@
 
 namespace sk::scaler
 {
+
+MinMaxScaler::MinMaxScaler(sk::Tensor &input) :
+    _min(sk::tensor::min(input, 1)), _max(sk::tensor::max(input, 1))
+
+{
+}
+
 sk::Tensor MinMaxScaler::transform(sk::Tensor &input)
 {
-    sk::Tensor min = sk::tensor::min(input, 1);
-    sk::Tensor max = sk::tensor::max(input, 1);
-    sk::Tensor output = input - min;
+    sk::Tensor output = input - this->_min;
 
-    output /= (max - min);
+    output /= (this->_max - this->_min);
+
+    return output;
+}
+
+sk::Tensor MinMaxScaler::inverse_transform(sk::Tensor &input)
+{
+    sk::Tensor scaled = (this->_max - this->_min);
+    scaled += this->_min;
+    sk::Tensor output = input.hadamard_dot(scaled);
 
     return output;
 }
--- sk-plusplus/sk-plusplus/scaler/standard_scaler.cc
+++ sk-plusplus/sk-plusplus/scaler/standard_scaler.cc
@@ -1,13 +1,29 @@
 #include "scaler.hh"
+#include "sk-plusplus/tensor/tensor.hh"
 
 namespace sk::scaler
 {
-sk::Tensor StandardScaler::transform(sk::Tensor &input) 
-{ 
-    sk::Tensor output = input - sk::tensor::mean(input, 1);
 
-    output /= sk::tensor::std(input, 1);
+StandardScaler::StandardScaler(sk::Tensor &input) :
+    _mean(sk::tensor::mean(input, 1)), _std(sk::tensor::std(input, 1))
+{
+}
 
-    return output; 
+sk::Tensor StandardScaler::transform(sk::Tensor &input)
+{
+    sk::Tensor output = input - this->_mean;
+
+    output /= this->_std;
+
+    return output;
+}
+
+sk::Tensor StandardScaler::inverse_transform(sk::Tensor &input)
+{
+    sk::Tensor output = input.hadamard_dot(this->_std);
+
+    output += this->_mean;
+
+    return output;
 }
 } // namespace sk::scaler
